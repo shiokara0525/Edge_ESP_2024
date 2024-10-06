4986e54ec8b7d8ae9f41e160a2e0d23cf0af4c23
yuta <yuta0525ny@gmail.com>
2024-10-06 18:04:29 +0900

    feat クラスを作った

---
+++ lib/teensy_send/teensy_send.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+#include<Arduino.h>
+
+class send_log{
+    public:
+    send_log(int sort_,int data_);
+    send_log(){};
+    void set_log(int sort_,int data_);
+
+    int sort = 0;
+    int send_data = 3838;
+};
+
+class send_teensy{
+    private:
+    send_log log[100];
+    int count = 0;
+    void send_onedata(int sort_,int data_);
+    void send_sixdata(int sort_,int data_[6]);
+
+    public:
+    send_teensy(int sort_,int data_);
+    void set_data(int sort_,int data_);
+    int send_data();
+};
---
+++ lib/teensy_send/tennsy_send.cpp
@@ -0,0 +1,42 @@
+#include<teensy_send.h>
+
+send_log::send_log(int sort_,int data_){
+    sort = sort_;
+    send_data = data_;
+}
+
+void send_log::set_log(int sort_,int data_){
+    sort = sort_;
+    send_data = data_;
+}
+
+send_teensy::send_teensy(int sort_,int data_){
+    log[count].set_log(sort_,data_);
+    count++;
+}
+
+void send_teensy::set_data(int sort_,int data_){
+    log[count].set_log(sort_,data_);
+    count++;
+}
+
+void send_teensy::send_onedata(int sort_,int data_){
+    uint8_t send_byte[5] = {38,0,0,0,37};
+    send_byte[1] = sort_;
+    send_byte[2] = byte( data_ >> 8 ); //ビットシフトで上位側の８Bitを取り出し、バイト型に型変換をする。
+    send_byte[3] = byte( data_ & 0xFF ); //論理和で下位側の８Bitを取り出し、バイト型に型変換をする。
+    Serial2.write(send_byte,5);
+}
+
+void send_teensy::send_sixdata(int sort_,int data_[6]){
+    for(int i = 0; i < 6; i++){
+        send_onedata(sort_ + i,data_[i]);
+    }
+}
+
+
+int send_teensy::send_data(){
+    for(int i = 0; i < count; i++){
+        send_onedata(log[i].sort,log[i].send_data);
+    }
+}
